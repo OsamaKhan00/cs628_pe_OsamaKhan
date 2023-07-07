ebe11b757a611b83f4594d25b1d55f94cf6d2171
OsamaKhan00 <97848125+OsamaKhan00@users.noreply.github.com>
2023-07-07 02:57:10 +0000

    Submission_OsamaKhan_PE01

---
+++ my-resume-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Resume from './Resume';
+
+function App() {
+  return (
+    <div className="app">
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
+
+
