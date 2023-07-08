5129a2dc3f595bd4f9abe5a7bcc081238b1103de
OsamaKhan00 <97848125+OsamaKhan00@users.noreply.github.com>
2023-07-08 06:01:05 +0000

    PE01_OsamaKhan

--- my-resume-app/src/Resume.js
+++ my-resume-app/src/Resume.js
@@ -1,12 +1,46 @@
+
 import React from 'react';
 import './Resume.css';
 
 function Resume() {
-  return (
-    <div className="resume">
-      <div className="Resume-css"> Hello</div>
-    </div>
-  );
+return (
+<div className="resume">
+<div className="Resume-css">
+<h1>Osama Khan</h1>
+<h2>Contact Information</h2>
+<p>Email: khanosama@cityuniversity.edu</p>
+<p>Phone: 00000000000</p>
+<h2>Summary</h2>
+<p>Highly motivated and eager to join this organization</p>
+<h2>Education</h2>
+<p>Bachelor of Science in Computer Science, City University of Seattle</p>
+<h2>Hands on</h2>
+<h3>Hands on:
+• In utilizing AWS cloud services to design and implement scalable and cost-effective solutions for diverse business
+needs.
+• In conducting vulnerability assessments and penetration testing on networks to identify and mitigate potential security
+risks.
+• In collaborated with team members to analyze and interpret security logs, identifying and responding to security
+incidents in a timely manner.
+• In the implementation and maintenance of security controls, such as firewalls, intrusion detection systems, and anti
+virus software.
+• In providing support in monitoring network traffic and analyzing security events using SIEM (Security Information and
+Event Management) tools.
+</h3>
+
+<h2>Skills</h2>
+<ul>
+<li>Programming Languages: JavaScript, Java, Python</li>
+<li>Web Development: HTML, CSS, React.js</li>
+<li>Database: SQL, MongoDB</li>
+<li>Version Control: Git</li>
+<li>Problem Solving</li>
+<li>Teamwork</li>
+<li>Effective Communication</li>
+</ul>
+</div>
+</div>
+);
 }
 
 export default Resume;
